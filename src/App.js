87f95b8be9726773c214398131dc861557a4cd71
rafaellore <birdneon3@gmail.com>
2022-09-10 15:54:07 -0300

    feat: removes  the components to implement the studies in property branches

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
 import React from 'react';
-import { Div } from './components/Div';
-import { H1 } from './components/H1';
-import { AppContext } from './contexts/app';
 
 function App() {
-  return (
-    <AppContext>
-      <Div>
-        <H1 />
-      </Div>
-    </AppContext>
-  );
+  return <div>Template</div>;
 }
 
 export default App;
--- src/components/Div/index.jsx
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line react/prop-types
-export const Div = ({ children }) => {
-  return <div>{children}</div>;
-};
